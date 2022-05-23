7ec8a79c9daf43a1eac9c4355d05922da71d7b38
cf19ArnauCastillo <cf19arnau.castillo@iesjoandaustria.org>
2022-05-23 15:52:18 +0200

    saldo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,13 +56,15 @@
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Saldo: 50€
                                     Benvingut,
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                        document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Tancar Sessió') }}
                                     </a>
 
